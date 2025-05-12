de2af37f1115583fa959c2a3686a95bc9d401bc8
afsalahamada-ac9 <afsalahamad@abovecloud9.ai>
2025-05-12 14:32:21 +0530

    test commit

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 func hglloHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
 	fmt.Println(name)
+	fmt.Sprintf("hwello woeld")
 }
 func main() {
 	http.HandleFunc("/hello", hglloHandler)
